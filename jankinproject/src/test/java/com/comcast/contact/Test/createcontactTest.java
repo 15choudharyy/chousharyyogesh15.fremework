fb777a84460f1c1aa656c763928fa8ff428e286b
city <city@city-PC>
2021-03-15 12:58:10 +0530

    commit

---
+++ jankinproject/src/test/java/com/comcast/contact/Test/createcontactTest.java
@@ -0,0 +1,25 @@
+package com.comcast.contact.Test;
+
+import org.testng.annotations.Test;
+
+public class createcontactTest {
+	
+	@Test
+	public void Createcontact(){
+		
+		System.out.println("hello");
+	}
+	@Test
+	public void modfiCreatecontact()
+	{
+		System.out.println("hello");
+	}
+		
+		@Test
+		public void deleteCreatecontact(){
+			System.out.println("hello");
+			
+		}
+			
+
+}
